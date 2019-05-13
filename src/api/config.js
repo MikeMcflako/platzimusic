7020bdc0b4bb4ed4d2f823534fea30357c3ddc31
MikeMcflako <mike.mcflako@gmail.com>
2019-05-13 00:11:50 -0500

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+// Application name	platzi-music
+// API key	85db169c54b70943c8d904687c3e31d2
+// Shared secret	d50b516b0df34b65ebb28e2c8fecc53d
+// Registered to	MikeMcflako
+
+export default {
+    appname: 'platzi-music',
+    apikey: '85db169c54b70943c8d904687c3e31d2',
+    secret: 'd50b516b0df34b65ebb28e2c8fecc53d',
+    registered: 'MikeMcflako'
+}
---
+++ src/api/index.js
@@ -0,0 +1,9 @@
+import config from './config';
+const URL = `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=:country&api_key=${config.apikey}&format=json`;
+
+export default function getArtists (country) {
+    const newUrl=URL.replace(':country',country);
+    return fetch(newUrl)
+        .then(res => res.json())
+        .then(json=>json.topartists.artist)
+}
