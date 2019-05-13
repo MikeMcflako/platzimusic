5feb3b1f905e8beeb8acd5e57363d565ddec407f
MikeMcflako <mike.mcflako@gmail.com>
2019-05-13 00:32:06 -0500

    Se corrigen rutas

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import config from './config';
-const URL = `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=:country&api_key=${config.apikey}&format=json`;
+const URL = `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=:country&api_key=${config.apikey}&format=json`;
 
 export default function getArtists (country) {
     const newUrl=URL.replace(':country',country);
